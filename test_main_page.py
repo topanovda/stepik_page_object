1dfd09adbeea2415c9f6c6d9d83b66091a66623f
Topanov Dmitrii <topanovda@lad24.ru>
2023-09-29 10:37:53 +0300

    добавлены тесты страницы логина

--- test_main_page.py
+++ test_main_page.py
@@ -19,13 +19,13 @@ from .pages.main_page import MainPage
 #     go_to_login_page(driver)
 
 
-# def test_guest_can_go_to_login_page(driver):
-#     link = "http://selenium1py.pythonanywhere.com/"
-#     page = MainPage(
-#         driver, link
-#     )  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-#     page.open()  # открываем страницу
-#     page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+def test_guest_can_go_to_login_page(driver):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(
+        driver, link
+    )  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()  # открываем страницу
+    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
 
 
 def test_guest_should_see_login_link(driver):
